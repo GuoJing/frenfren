2aa907462e71f631f77900508e6f2cbcbb7ea85c
GuoJing <x@guojing.me>
2018-01-05 21:17:08 +0800

    init project

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,7 +1,32 @@
 const app = getApp();
 
 Page({
+  fillBg: function(event) {
+    var color = event.target.dataset.color;
+    this.setData({
+      avatarBg: color
+    });
+  },
+  fillAvatar: function(event) {
+    var src = event.target.dataset.src;
+    this.setData({
+      avatarImg: src
+    })
+  },
+  fillEnv: function (event) {
+    var src = event.target.dataset.src;
+    this.setData({
+      avatarEnv: src
+    })
+  },
+  fillExtra: function (event) {
+    var src = event.target.dataset.src;
+    this.setData({
+      avatarExtra: src
+    })
+  },
   data: {
-    message: '无 Liao 啊!'
+    avatarBg: '#f2f6f9',
+    avatarImg: 'http://7xll9d.com1.z0.glb.clouddn.com/wechat/app/2017-Q1-01-C.PNG'
   }
 });
